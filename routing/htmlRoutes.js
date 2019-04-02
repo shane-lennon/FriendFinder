07c07971750f58609208942d5f2cac15ecc98353
shane-lennon <shane.michael.lennon@gmail.com>
2019-04-01 20:20:59 -0500

    Adjusted path

--- routing/htmlRoutes.js
+++ routing/htmlRoutes.js
@@ -4,12 +4,12 @@ module.exports = function (app) {
 
     app.get("/", function (request, response) {
         console.log(request.url);
-        response.sendFile(path.join(__dirname, "/./public/index.html"));
+        response.sendFile(path.join(__dirname, "/../public/index.html"));
     });
 
     app.get("/survey", function (request, response) {
         console.log(request.url);
-        response.sendFile(path.join(__dirname, "/./public/survey.html"));
+        response.sendFile(path.join(__dirname, "/../public/survey.html"));
     });
 
     // Catch-all will send any other path back to main path
